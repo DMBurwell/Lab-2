4e19db5249bf8451d7406db098a2d55d1f3c717a
DMBurwell <danica.burwell@outlook.com>
2021-10-27 20:11:05 -0400

    2

---
+++ Utilites.R
@@ -0,0 +1,40 @@
+---
+  title: "Lab 2"
+author: "Danica Burwell"
+date: "10/27/2021"
+output: html_document
+---
+  
+  
+library(dplyr)
+library(here)
+library(tidyverse)
+
+
+DD_FILEPATH <- here::here( "C:/Users/danic/OneDrive/Documents/SCHOOL/Fall 2021 B/Lab 2/Data/Rodeo/ltdb-data-dictionary.csv" )
+
+# import data dictionary
+dd <- read.csv( DD_FILEPATH, stringsAsFactors=F )
+
+
+  
+#-------------------- 
+#Filter 1: Search by Category
+
+search_categories <- function(dd, categories) {search_categories <- grepl(categories, dd[categories], ignore.case = T)
+all_categories <- dd[
+  search_categories, c("age", "age-race", "age-ses", "edu", "employ", "employ-age", "fam", "fam-ses", "house", "lang", "pop", "pop-race", "race", "race-ses", "ses")]
+return(all_categories)}
+
+search_categories(dd$categories("age"))
+
+
+#--------------------
+
+#Filter 2: Search by Description
+
+
+
+#--------------------
+
+#Filter 3: Search by Time Period
